4c68b2c009d2937521a3c4716f5be98b59367aa6
alexx33915 <sash-2007@ya.ru>
2021-01-16 17:16:06 +0500

    5.2.2. Реализовать Store<T extends Base>[157#407563]

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -1,30 +1,67 @@
 package ru.job4j.generics;
 
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public final class MemStore<T extends Base> implements Store<T> {
-
+public final class MemStore<T extends Base>   implements Store<T>, Iterable<T> {
     private final List<T> mem = new ArrayList<>();
 
     @Override
     public void add(T model) {
-
+            mem.add(model);
     }
 
     @Override
-    public Boolean replace(String id, T model) {
+    public boolean replace(String id, T model) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                mem.remove(i);
+                mem.add(i, model);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean delete(String id) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                mem.remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
+
+
     @Override
     public T findById(String id) {
-        return null;
+        if (!checkId(id)) {
+            throw new NoSuchElementException("no id");
+        }
+        T t = null;
+        for (T value : mem) {
+            if (value.getId().equals(id)) {
+                t =  value;
+            }
+        }
+        return t;
+    }
+
+    public boolean checkId(String id) {
+
+        for (T t : mem) {
+            if (t.getId().equals(id)) {
+                return true;
+            }
+        }
+        return  false;
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return mem.iterator();
     }
 }
--- src/main/java/ru/job4j/generics/Store.java
+++ src/main/java/ru/job4j/generics/Store.java
@@ -3,7 +3,7 @@ package ru.job4j.generics;
 
 public interface Store<T extends Base> {
     void add(T model);
-    Boolean replace(String id, T model);
+    boolean replace(String id, T model);
     boolean delete(String id);
     T findById(String id);
 }
