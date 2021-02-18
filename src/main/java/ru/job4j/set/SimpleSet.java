8c6172f27b870f01079f88464e2f6ca5221a767e
alexx33915 <sash-2007@ya.ru>
2021-02-18 15:32:44 +0500

    1. Реализовать коллекцию Set на массиве[996#407566]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -8,49 +8,32 @@ public class SimpleSet<T> implements Iterable<T> {
 
     SimpleArray<T>  simpleArray = new SimpleArray<>();
 
-    public boolean add(T t) {
-        boolean bool = true;
-        for (T t1 : simpleArray) {
-            if (t1.equals(t)) {
-                bool = false;
+    public void add(T t) {
+        boolean isNewElement = true;
+
+        for (T item : simpleArray) {
+            if (item == null) {
+                if (t == null) {
+                    isNewElement = false;
+                    break;
+                }
+            } else if (item.equals(t)) {
+                isNewElement = false;
                 break;
             }
         }
-        if (bool) {
+
+        if (isNewElement) {
             simpleArray.add(t);
-            return true;
-        } else {
-            return false;
         }
     }
+
 
     @Override
     public Iterator<T> iterator() {
         return simpleArray.iterator();
     }
 
-    public static void main(String[] args) {
-
-//
-//        List<String> expected = List.of("ss", "dd", "bb");
-//        SimpleSet<String> set = new SimpleSet<>();
-//        expected.forEach(set::add);
-//
-//        List<String> actual = new ArrayList<>();
-//
-//        for (String str : set) {
-//            actual.add(str);
-
-        SimpleSet<String> ss = new SimpleSet<>();
-     //   List<String> str = List.of("aaa", "bbb", "ccc", "aaa", null, null);
-     //   ss.addAll(str);
-        ss.add(null);
-      //  ss.add(null);
-        System.out.println(ss);
-
-
-
-        }
     }
 
 
--- src/test/java/ru/job4j/set/SimpleSetTest.java
+++ src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -27,5 +27,32 @@ public class SimpleSetTest {
         assertThat(Arrays.asList(1, 2, 3), is(result));
     }
 
+    @Test
+    public void whenAddNullElement (){
+        SimpleSet<String> simpleSet = new SimpleSet<>();
+       simpleSet.add(null);
+       simpleSet.add(null);
+       simpleSet.add(null);
 
+        List<String> result = new ArrayList<>();
+        for (String str: simpleSet) {
+            result.add(str);
+        }
+        List<String> actual = new ArrayList<>();
+        actual.add(null);
+        assertThat(actual, is(result));
+    }
+
+    @Test
+    public void whenAdd1null2null3ShouldGe1null2null3() {
+        List<Integer> expected = Arrays.asList(1, null, 2 , null , 3);
+        SimpleSet<Integer> set = new SimpleSet<>();
+        expected.forEach(set::add);
+        List<Integer> actual = new ArrayList<>();
+        for (Integer value: set) {
+            actual.add(value);
+        }
+        List<Integer> expected2 = Arrays.asList(1, null, 2  , 3);
+        assertThat(actual, is(expected2));
+    }
 }
