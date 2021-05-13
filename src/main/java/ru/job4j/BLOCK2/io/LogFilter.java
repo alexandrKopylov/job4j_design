0a6b8865b51f21cf7617331992296744db17af46
alexx33915 <sash-2007@ya.ru>
2021-05-13 10:25:44 +0500

    0.4. BufferedOutputStream [#252490 #237869]

--- src/main/java/ru/job4j/BLOCK2/io/LogFilter.java
+++ src/main/java/ru/job4j/BLOCK2/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.BLOCK2.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -13,21 +12,33 @@ public class LogFilter {
     public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                if (  pattern.matcher(line).matches()) {
+                if (pattern.matcher(line).matches()) {
                     list.add(line);
                 }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         return list;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String str : log) {
+                out.printf("%s%n", str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }
