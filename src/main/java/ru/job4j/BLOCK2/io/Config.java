153557420aea483248b746983f96dad62349702b
alexx33915 <sash-2007@ya.ru>
2021-05-17 19:11:24 +0500

    Читаем файл конфигурации[858#407595]-2

--- src/main/java/ru/job4j/BLOCK2/io/Config.java
+++ src/main/java/ru/job4j/BLOCK2/io/Config.java
@@ -2,29 +2,43 @@ package ru.job4j.BLOCK2.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
 public class Config {
     private final String path;
     private final Map<String, String> values = new HashMap<String, String>();
+    private static Pattern notKey = Pattern.compile("\\s*=.*");
+    private static Pattern notValue = Pattern.compile(".*=\\s*");
 
     public Config(final String path) {
         this.path = path;
     }
 
+
     public void load() {
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
             read.lines()
-                    .filter(s -> (!s.isEmpty() || !s.startsWith("#")))
-                    .filter(s -> s.contains("="))
+                    .filter(Config::stringsFilter)
                     .forEach(s -> values.put(s.substring(0, s.indexOf("=")), s.substring(s.indexOf("=") + 1))
                     );
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
+    private static boolean stringsFilter(String str) {
+        if (str.isEmpty() || str.startsWith("#")) {
+            return false;
+        }
+        if (!str.contains("=") || notKey.matcher(str).matches() || notValue.matcher(str).matches()) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
 
     public String value(String key) {
         return values.get(key);
@@ -40,8 +54,10 @@ public class Config {
         }
         return out.toString();
     }
-    public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
-    }
 
+
+    public static void main(String[] args) {
+        Config cfg = new Config("app.properties");
+
+    }
 }
--- src/test/java/ru/job4j/BLOCK2/io/ConfigTest.java
+++ src/test/java/ru/job4j/BLOCK2/io/ConfigTest.java
@@ -16,4 +16,18 @@ public class ConfigTest {
         assertThat(config.value("hibernate.connection.url"),is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
         assertThat(config.value("hibernate.connection.driver_class"),is("org.postgresql.Driver"));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotKey() {
+        String path = "app1.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotValue() {
+        String path = "app2.properties";
+        Config config = new Config(path);
+        config.load();
+    }
 }
