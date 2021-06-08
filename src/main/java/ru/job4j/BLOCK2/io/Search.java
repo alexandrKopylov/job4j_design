6d648e2371e953115d6a8eafb27838be46134769
alexx33915 <sash-2007@ya.ru>
2021-06-08 18:25:20 +0500

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/BLOCK2/io/Search.java
@@ -0,0 +1,23 @@
+package ru.job4j.BLOCK2.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        String str = "c:\\Program Files\\AutoCAD 2010";
+        Predicate<Path> predicate = p -> p.toFile().getName().endsWith("exe");
+        Path root = Paths.get(str);
+        search(root, predicate).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> predicate) throws IOException {
+        PrintFiles searcher = new PrintFiles(predicate);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
