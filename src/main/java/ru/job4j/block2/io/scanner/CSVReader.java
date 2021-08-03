35afd69f1cff93249f27aefb1f878b20dac63e4b
alexandrKopylov <sash-2007@ya.ru>
2021-08-03 17:57:23 +0500

    7. Scanner [#504791 #183268]-4

--- src/main/java/ru/job4j/block2/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/block2/io/scanner/CSVReader.java
@@ -3,7 +3,6 @@ package ru.job4j.block2.io.scanner;
 import ru.job4j.block2.io.ArgsName;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.StringJoiner;
@@ -11,22 +10,29 @@ import java.util.regex.Pattern;
 
 public class CSVReader {
     public static void main(String[] args) throws FileNotFoundException {
-
         if (args.length != 4) {
             throw new IllegalArgumentException("wrong count arguments");
         }
         CSVReader csvr = new CSVReader();
-
         ArgsName csvArgs = ArgsName.of(args);
         String arg1 = csvArgs.get("path");
         String arg2 = csvArgs.get("out");
         String arg3 = csvArgs.get("delimiter");
         String arg4 = csvArgs.get("filter");
 
-         csvr.validation(arg1, arg2, arg4);
-        String strBild = csvr.reader( arg1, arg3, arg4);
+        csvr.validation(arg1, arg2, arg4);
+        String[] columns = csvr.getColumns(arg1, arg3);
+        String strBild = csvr.reader(columns, arg1, arg3, arg4);
         out(arg2, strBild);
+    }
 
+    private String[] getColumns(String arg1, String arg3) throws FileNotFoundException {
+        String[] mas = null;
+        Scanner sc = new Scanner(new File(arg1));
+        if (sc.hasNextLine()) {
+            mas = sc.nextLine().split(arg3);
+        }
+        return mas;
     }
 
     private static void out(String arg2, String strBild) {
@@ -68,50 +74,22 @@ public class CSVReader {
         }
     }
 
-    private String reader( String path, String delimiter, String filter) throws FileNotFoundException {
-
-       // StringBuilder strBild = new StringBuilder();
-        //strBild.append(System.lineSeparator());
+    private String reader(String[] columns, String path, String delimiter, String filter) throws FileNotFoundException {
         Pattern ptn = Pattern.compile("\\n|" + delimiter);
         Scanner sc = new Scanner(new File(path)).useDelimiter(ptn);
         StringJoiner line = new StringJoiner(System.lineSeparator());
-
-        int n = 0;
         String token;
-
         while (sc.hasNext()) {
             StringJoiner joiner = new StringJoiner(delimiter);
-            token = sc.next().trim();
-            if (filter.contains(StructuraCSV.name.toString())   && n == 0) {
-                joiner.add(token);
+            for (int i = 0; i < columns.length; i++) {
+                token = sc.next().trim();
+                if (filter.contains(columns[i])) {
+                    joiner.add(token);
+                }
             }
-            n++;
-            token = sc.next().trim();
-            if (filter.contains(StructuraCSV.age.toString())   && n == 1) {
-                joiner.add(token);
-            }
-            n++;
-            token = sc.next().trim();
-            if (filter.contains(StructuraCSV.birthDate.toString())   && n == 2) {
-                joiner.add(token);
-            }
-            n++;
-            token = sc.next().trim();
-            if (filter.contains(StructuraCSV.education.toString())   && n == 3) {
-                joiner.add(token);
-            }
-            n++;
-            token = sc.next().trim();
-            if (filter.contains(StructuraCSV.children.toString())   && n == 4) {
-                joiner.add(token);
-            }
-            n++;
-            if (n == 5) {
-                line.add(joiner.toString());
-                n = 0;
-            }
+            line.add(joiner.toString());
         }
-
         return line.toString();
     }
+
 }
--- src/main/java/ru/job4j/block2/io/scanner/StructuraCSV.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.block2.io.scanner;
-
-public enum StructuraCSV {
-    name,
-     age,
-     birthDate,
-     education,
-     children
-}
