a5a4ae9b2593906d3e7b57f557e164388fc82bbe
Sasha <sash-2007@ya.ru>
2021-11-08 08:11:44 +0500

    examen SQL-7

--- src/main/java/ru/job4j/block3/jdbc/StatementDemo.java
+++ src/main/java/ru/job4j/block3/jdbc/StatementDemo.java
@@ -7,7 +7,7 @@ public class StatementDemo {
 
     private static Connection getConnection() throws Exception {
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql:
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
         String login = "postgres";
         String password = "password";
         return DriverManager.getConnection(url, login, password);
