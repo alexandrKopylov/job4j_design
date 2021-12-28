30b1d3345e9a0ecd91cb1264900858e1878ca8ec
Sasha <sash-2007@ya.ru>
2021-12-28 12:18:22 +0500

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/block5/template/ClassGeneratorTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.block5.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ClassGeneratorTest {
+
+    @Test
+    public void whenTemplateWorkCorrect() {
+        ClassGenerator cg = new ClassGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        String result = cg.produce(template, map);
+        Assert.assertThat(result, is("I am a Petr Arsentev, Who are you? "));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBadTemplate() {
+        ClassGenerator cg = new ClassGenerator();
+        String template = "I am a {name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        String result = cg.produce(template, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBadKeyInMap() {
+        ClassGenerator cg = new ClassGenerator();
+        String template = "I am a {name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("nam", "Petr Arsentev");
+        map.put("subject", "you");
+        String result = cg.produce(template, map);
+    }
+}
